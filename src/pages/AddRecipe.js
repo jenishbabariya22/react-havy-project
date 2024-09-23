b866ed71c5442a2818bd3eb33516dba27db3e023
jenishbabariya22 <131512066+jenishbabariya22@users.noreply.github.com>
2024-09-24 02:02:54 +0530

    Update AddRecipe.js

--- src/pages/AddRecipe.js
+++ src/pages/AddRecipe.js
@@ -66,7 +66,7 @@ const AddRecipe = ({ addRecipe }) => {
         required
       />
       <input
-        type="text"
+        type="file"
         className="border p-2 w-full mb-4"
         placeholder="Image URL"
         value={imageUrl}
